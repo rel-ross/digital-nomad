6d2ded7aa83e7668e421270acae5e34519a7b934
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-01-26 15:56:12 -0700

    Can search a city name and all campsites in that city will come up

--- frontend/searchedCampsite.js
+++ frontend/searchedCampsite.js
@@ -1,5 +1,10 @@
+const searchParams = new URLSearchParams(window.location.search)
+const search_term = searchParams.get("search_term")
+
+console.log(search_term)
+
 const $campsiteSearchResult = document.querySelector(".campsite-search-result")
-fetch("http://localhost:3000/campsites")
+fetch(`http://localhost:3000/campsites?search_term=${search_term}`)
    .then(response => response.json())
    .then(campsites => {
       campsites.map(campsite =>{
