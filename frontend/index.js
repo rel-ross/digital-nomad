e42cdc6ec74d489dd6d24c426866aab824dcb10b
Nathan <n.gusky88@gmail.com>
2021-01-27 11:10:32 -0700

    login register close

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,10 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
+const center = document.getElementById('btn');
+const left = document.getElementById('login');
+const right = document.getElementById('register');
+
 openModalButtons.forEach( button => {
    button.addEventListener('click', () => {
       const openmodal = document.querySelector('#open-modal')
@@ -33,6 +37,20 @@ function closeModal(modal) {
    modal.classList.remove('active')
    overlay.classList.remove('active')
 };
+
+function login() {
+   center.style.left = "0px"
+   left.style.left = "450px"
+   right.style.left = "50"
+};
+
+function register() {
+   center.style.left = "100px"
+   left.style.left = "50px"
+   right.style.left = "-450"
+}
+
+
 
 
 
